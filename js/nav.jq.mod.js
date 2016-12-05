787c2ed2a317985f824c61425e643bb3afd1e99c
N REZA <nreza@Ns-MacBook-Pro.local>
2016-12-06 01:36:27 +0600

    All local files included

---
+++ js/nav.jq.mod.js
@@ -0,0 +1,15 @@
+jQuery(function(){
+    jQuery('#navBttn').on('click', function() {
+      jQuery('#navBox').slideDown(250);
+
+    }
+    );
+jQuery('#navBttnX').on('click', function() {
+      jQuery('#navBox').slideUp(250);
+
+    }
+    );
+
+
+}
+);
---
+++ js/navFader.js
@@ -0,0 +1,15 @@
+jQuery(function(){
+    jQuery('#navBttn').on('click', function() {
+      jQuery('#navBox').fadeIn(250);
+
+    }
+    );
+jQuery('#navBttnX').on('click', function() {
+      jQuery('#navBox').fadeOut(250);
+
+    }
+    );
+
+
+}
+);
